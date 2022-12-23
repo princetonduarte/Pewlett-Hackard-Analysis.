65db728f1747d62d3df9b7c16976cf881dda4b14
Princeton Duarte <115187898+princetonduarte@users.noreply.github.com>
2022-12-22 18:58:04 -0600

    Update Employee_Database_challenge.sql

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,1 +1,12 @@
-
+SELECT e.emp_no,
+       e.first_name,
+       e.last_name,
+       t.title,
+       t.from_date,
+       t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+order by e.emp_no;
